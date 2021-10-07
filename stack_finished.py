e50c2168402b6afbc16b64a6fbe4a3c248be58f4
bassemhussein-code <75940885+bassemhussein-code@users.noreply.github.com>
2021-10-07 18:31:03 +0200

    Add files via upload

---
+++ stack_finished.py
@@ -0,0 +1,18 @@
+# try out the Python stack functions
+
+# create a new empty stack
+stack = []               # in real .. this is an empty list . 
+
+# push items onto the stack
+stack.append(1)                         # list's method (append) 
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+# print the stack contents
+print(stack)
+
+# pop an item off the stack
+x = stack.pop()                         # list's method (pop)
+print(x)
+print(stack)
